015f65ebbac18a726324ca15e3883c5bb2692648
Sahil-Gujrati <sahilgujrati12@gmail.com>
2025-06-26 16:48:03 +0530

    feat: Update loops

--- src/protocols/loops/PositionsLoops.sol
+++ src/protocols/loops/PositionsLoops.sol
@@ -13,8 +13,9 @@ import {EnumerableSet} from "@openzeppelin-contracts-5.3.0/utils/structs/Enumera
 import {IBerachainRewardsVault} from "../../interfaces/handlers/pol/IBerachainRewardsVault.sol";
 import {IPriceOracle} from "../../interfaces/oracle/IPriceOracle.sol";
 import {IPositionsRelayer} from "../../interfaces/poc/IPositionsRelayer.sol";
-import {IV2DexPair} from "@src/interfaces/protocols/loops/IV2DexPair.sol";
-import {IV2DexRouter} from "@src/interfaces/protocols/loops/IV2DexRouter.sol";
+import {IIslandRouter, RouterSwapParams} from "@src/interfaces/protocols/loops/IIslandRouter.sol";
+import {IKodiakIsland} from "@src/interfaces/protocols/loops/IKodiakIsland.sol";
+import {ISwapRouter} from "@src/interfaces/protocols/loops/ISwapRouter.sol";
 
 import {PositionsBGTHandler} from "../../handlers/pol/PositionsBGTHandler.sol";
 import {PositionsLendingPool} from "@src/protocols/lendingPool/PositionsLendingPool.sol";
@@ -40,11 +41,11 @@ contract PositionsLoops is Initializable, UUPSUpgradeable, AccessControlUpgradea
         uint256 tokenId;
         address token;
         uint256 amount;
+        bool zeroForOne;
         uint256 minSwapAmounOut;
-        uint256 deadline;
-        uint256 minAmountBorrowToken;
-        uint256 minAmountOtherToken;
+        bytes routeData;
         uint256 minLpTokensToReceive;
+        uint160 maxSlippage;
     }
 
     struct PositionData {
@@ -64,7 +65,9 @@ contract PositionsLoops is Initializable, UUPSUpgradeable, AccessControlUpgradea
         uint256 amountTokenMin;
         uint256 amountOtherTokenMin;
         uint256 minSwapAmountOut;
+        uint24 fee;
         uint256 deadline;
+        uint160 sqrtPriceLimitX96;
     }
 
     /// @dev Relayer role can call relayer functions.
@@ -77,9 +80,10 @@ contract PositionsLoops is Initializable, UUPSUpgradeable, AccessControlUpgradea
     address public relayer;
     address public lendingPool;
     address public oracle;
-    address public v2DexRouter;
-    address public pool;
+    address public islandRouter;
+    address public island;
     address public vault;
+    address public swapRouter;
     /// @notice Tracking used nonces for each proof of collateral Nft tokenId.
     mapping(uint256 tokenId => uint256 nonces) public nonces;
     /// @notice Tracking data for each withdrawal request.
@@ -103,8 +107,9 @@ contract PositionsLoops is Initializable, UUPSUpgradeable, AccessControlUpgradea
         address _positionsRelayer,
         address _priceOracle,
         address _lendingPool,
-        address _v2DexRouter,
-        address _pool,
+        address _islandRouter,
+        address _island,
+        address _swapRouter,
         address _vault,
         address _bgt
     ) public initializer {
@@ -117,11 +122,10 @@ contract PositionsLoops is Initializable, UUPSUpgradeable, AccessControlUpgradea
         relayer = _positionsRelayer;
         oracle = _priceOracle;
         lendingPool = _lendingPool;
-        v2DexRouter = _v2DexRouter;
-        pool = _pool;
+        islandRouter = _islandRouter;
+        island = _island;
+        swapRouter = _swapRouter;
         vault = _vault;
-
-        IERC20(pool).approve(vault, type(uint256).max);
     }
 
     function requestOpenLeveragedPosition(
@@ -129,15 +133,18 @@ contract PositionsLoops is Initializable, UUPSUpgradeable, AccessControlUpgradea
         address _borrowToken,
         uint256 _amount,
         uint256 _minSwapAmountOut,
-        uint256 _deadline,
-        uint256 _minAmountBorrowToken,
-        uint256 _minAmountOtherToken,
+        bool _zeroForOne,
+        bytes memory _routeData,
         uint256 _minLpTokensToReceive,
+        uint160 _maxSlippage,
         bytes32[] memory _proof
     ) external {
         _validateNFTOwnership(_tokenId, _proof);
 
-        if (_borrowToken != IV2DexPair(pool).token0() && _borrowToken != IV2DexPair(pool).token1()) {
+        if (
+            _borrowToken != address(IKodiakIsland(island).token0())
+                && _borrowToken != address(IKodiakIsland(island).token1())
+        ) {
             revert PositionsLoops__InvalidToken();
         }
 
@@ -147,11 +154,11 @@ contract PositionsLoops is Initializable, UUPSUpgradeable, AccessControlUpgradea
             tokenId: _tokenId,
             token: _borrowToken,
             amount: _amount,
+            zeroForOne: _zeroForOne,
             minSwapAmounOut: _minSwapAmountOut,
-            deadline: _deadline,
-            minAmountBorrowToken: _minAmountBorrowToken,
-            minAmountOtherToken: _minAmountOtherToken,
-            minLpTokensToReceive: _minLpTokensToReceive
+            routeData: _routeData,
+            minLpTokensToReceive: _minLpTokensToReceive,
+            maxSlippage: _maxSlippage
         });
 
         emit RequestOpenLeveragedPosition(msg.sender, _tokenId, _borrowToken, _amount);
@@ -177,34 +184,19 @@ contract PositionsLoops is Initializable, UUPSUpgradeable, AccessControlUpgradea
 
         PositionsLendingPool(lendingPool).borrowForLoops(positionRequestData.token, positionRequestData.amount);
 
-        address otherToken = positionRequestData.token == IV2DexPair(pool).token0()
-            ? IV2DexPair(pool).token1()
-            : IV2DexPair(pool).token0();
-
-        address[] memory path = new address[](2);
-        path[0] = positionRequestData.token;
-        path[1] = otherToken;
-
-        IERC20(positionRequestData.token).approve(v2DexRouter, positionRequestData.amount);
-        IV2DexRouter(v2DexRouter).swapExactTokensForTokens(
-            positionRequestData.amount / 2,
-            positionRequestData.minSwapAmounOut,
-            path,
-            address(this),
-            positionRequestData.deadline
-        );
-
-        uint256 balance = IERC20(otherToken).balanceOf(address(this));
-        IERC20(otherToken).approve(v2DexRouter, balance);
-        (,, uint256 lpTokens) = IV2DexRouter(v2DexRouter).addLiquidity(
-            positionRequestData.token,
-            otherToken,
-            positionRequestData.amount / 2,
-            balance,
-            positionRequestData.minAmountBorrowToken,
-            positionRequestData.minAmountOtherToken,
-            address(this),
-            positionRequestData.deadline
+        IERC20(positionRequestData.token).approve(islandRouter, positionRequestData.amount);
+        (,, uint256 lpTokens) = IIslandRouter(islandRouter).addLiquiditySingle(
+            IKodiakIsland(island),
+            positionRequestData.amount,
+            positionRequestData.minLpTokensToReceive,
+            positionRequestData.maxSlippage,
+            RouterSwapParams({
+                amountIn: positionRequestData.amount / 2,
+                minAmountOut: positionRequestData.minSwapAmounOut,
+                zeroForOne: positionRequestData.zeroForOne,
+                routeData: positionRequestData.routeData
+            }),
+            address(this)
         );
 
         uint256 currentBorrowIndex =
@@ -233,28 +225,32 @@ contract PositionsLoops is Initializable, UUPSUpgradeable, AccessControlUpgradea
         IBerachainRewardsVault(vault).withdraw(_params.amountLpTokens);
         IERC20(_params.token).safeTransferFrom(msg.sender, address(this), _params.buffer);
 
-        address otherToken =
-            _params.token == IV2DexPair(pool).token0() ? IV2DexPair(pool).token1() : IV2DexPair(pool).token0();
+        address otherToken = _params.token == address(IKodiakIsland(island).token0())
+            ? address(IKodiakIsland(island).token1())
+            : address(IKodiakIsland(island).token0());
 
-        IERC20(pool).approve(v2DexRouter, _params.amountLpTokens);
-        IV2DexRouter(v2DexRouter).removeLiquidity(
-            _params.token,
-            otherToken,
+        IERC20(island).approve(islandRouter, _params.amountLpTokens);
+        IIslandRouter(islandRouter).removeLiquidity(
+            IKodiakIsland(island),
             _params.amountLpTokens,
             _params.amountTokenMin,
             _params.amountOtherTokenMin,
-            address(this),
-            _params.deadline
+            address(this)
         );
 
-        address[] memory path = new address[](2);
-        path[0] = otherToken;
-        path[1] = _params.token;
-
         uint256 amount = IERC20(otherToken).balanceOf(address(this));
-        IERC20(otherToken).approve(v2DexRouter, amount);
-        IV2DexRouter(v2DexRouter).swapExactTokensForTokens(
-            amount, _params.minSwapAmountOut, path, address(this), _params.deadline
+        IERC20(otherToken).approve(swapRouter, amount);
+        ISwapRouter(swapRouter).exactInputSingle(
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: otherToken,
+                tokenOut: _params.token,
+                fee: _params.fee,
+                recipient: msg.sender,
+                deadline: _params.deadline,
+                amountIn: amount,
+                amountOutMinimum: _params.amountOtherTokenMin,
+                sqrtPriceLimitX96: _params.sqrtPriceLimitX96
+            })
         );
 
         uint256 currentBorrowIndex = PositionsLendingPool(lendingPool).getReserveData(_params.token).borrowIndex;
